3183cb049a8b3da86ebe60a954a841d83603fdee
Dmitry Petukhov <dmitry.petukhov@akvelon.com>
2023-03-17 13:15:32 +0600

    WIP update yAxisUtils

--- src/utils/axis/yAxisUtils.ts
+++ src/utils/axis/yAxisUtils.ts
@@ -1,4 +1,3 @@
-module powerbi.extensibility.visual.xAxisUtils {
-    export const getXAxisMaxWidth = (visualWidth, settings) => ((visualWidth) / 100) * settings.categoryAxis.maximumSize;
-}
+import { VisualSettings } from "../../settings";
 
+export const getXAxisMaxWidth = (visualWidth: number, settings: VisualSettings): number => ((visualWidth) / 100) * settings.categoryAxis.maximumSize;
